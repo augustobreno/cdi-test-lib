c0c3fc3946e53a8ed10de7166a29daa84f65ee7c
augusto <augusto.breno@trt6.gov.br>
2015-09-16 13:16:01 -0300

    Removendo o Arquillian das dependências do projeto.

---
+++ sources/vcgerente-tests/src/test/java/br/com/vocegerente/vcgerente/tests/_AppSuite.java
@@ -0,0 +1,11 @@
+package br.com.vocegerente.vcgerente.tests;
+
+import br.com.vocegerente.vcgerente.tests.suite.SuiteBase;
+
+/**
+ * Testes do pacote de dominio. 
+ * @author Augusto
+ */
+public class _AppSuite extends SuiteBase {
+
+}
